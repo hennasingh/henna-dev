a3028aaadd958f00a2d1aba606ab497591a35735
Henna Singh <hennasingh04@gmail.com>
2024-02-26 08:34:49 +0000

    added animation and styles to hero-text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,13 @@ export default function Home() {
     return (
         <section className='home-hero'>
             <div className='home-hero-content'>
-                <h1 className='hero-heading'>Hey, Welcome to my creative page</h1>
+                <h1 className='hero-heading'>Hey, Welcome to my Creative Nook!</h1>
                 <div className='hero-body'>
                     <p className='hero-text'>
-                        I am a passionate technical community builder, educator, writer,
-                        speaker, developer and a continuous learner based in Dublin, Ireland.
-                        Currently, leveling up my knowledge in React JS and Tailwind CSS to transition to a front-end developer role.
+                        I am a passionate technical <span>community builder,</span>  educator, writer, 
+                      <span> speaker, developer</span> and a continuous learner based in Dublin, Ireland.
+                        Currently, leveling up my knowledge in <span>React JS</span> and <span>Tailwind CSS</span> 
+                        to transition to a front-end developer role.
                     </p>
                 </div>
                 <a href='./#projects' className='projects-btn'>See my projects</a>
