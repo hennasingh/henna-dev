04b560ae3375fccdb8a78c91c9e8fc3bc87436ad
Henna Singh <hennasingh04@gmail.com>
2024-03-24 06:16:59 +0530

    edited ssome header and hero styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,6 @@ export default function Header() {
     const Menu = () => (
         <nav>
             <ul>
-                <li><a href="#home">Home</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
