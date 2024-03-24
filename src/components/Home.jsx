3e437fb1a0e7be67cd7f7edb514e35dd4b7557f5
Henna Singh <hennasingh04@gmail.com>
2024-03-24 12:42:14 +0530

    added image to hero section and edited styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import aboutMe from '../assets/aboutMe.jpeg'
 
 export default function Home() {
 
     return (
         <section className='home-hero'>
             <div className='home-hero-content'>
-                <h1 className='hero-heading'>Hey, Welcome to my Creative Nook!</h1>
-                <div className='hero-body'>
+                <div>
+                    <h1 className='hero-heading'>Hey, Welcome to my Creative Nook!</h1>
                     <p className='hero-text'>
                         I am a passionate technical <span>community builder,</span>  educator, writer, 
-                      <span> speaker, developer</span> and a continuous learner based in Dublin, Ireland.
+                        <span> speaker, developer</span> and a continuous learner based in Dublin, Ireland.
                         Currently, building web solutions in <span>React JS</span> , HTML, CSS, and <span> Firebase </span> 
                         to transition to a front-end developer role.
                     </p>
+                    <a href='./#projects' className='projects-btn'>See my projects</a>  
                 </div>
-                <a href='./#projects' className='projects-btn'>See my projects</a>
-            </div>           
+                <img src= {aboutMe} alt="henna standing at podium talking"/>
+            </div>   
+      
         </section>
     )
 }
