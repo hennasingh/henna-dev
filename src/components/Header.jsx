881fccc17426bf9eaea47ea769d122d5297948f1
Henna Singh <hennasingh04@gmail.com>
2024-03-29 20:06:52 +0530

    implemented separate Menu component and added styles to open and close menu and made header fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import logo from '../assets/thumbnail.jpeg'
+import React from "react";
+import logo from "../assets/thumbnail.jpeg";
 import { CgMenuRight, CgClose } from "react-icons/cg";
+import Menu from "./Menu";
 
 export default function Header() {
+  const [showMenu, setShowMenu] = React.useState(false);
 
-    const [showMenu, setShowMenu] = React.useState(false)
+  const toggleMenu = () => {
+    setShowMenu((prevMenu) => !prevMenu);
+  };
 
-    const toggleMenu = () => {
-        setShowMenu(!showMenu)
-    }
-
-    const Menu = () => (
-        <nav>
-            <ul className={showMenu ? 'ul-item': ''}>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
-    )
-
-    return (
-        <header>
-            <div className='header-container'>
-                <div>
-                    <img className="header-logo"src={logo} alt="thumbnail of henna singh" />
-                    <p>Henna Singh</p>
-                </div>
-                <div className='menu'>
-                    {
-                        showMenu ? (
-                            <CgClose size={36} color='#EA035E' onClick={toggleMenu} className='close-menu'/>
-                        ) 
-                        : (
-                            <CgMenuRight size={36} color='#EA035E'onClick={toggleMenu} className='menu'/>
-                        )
-
-                    }
-                    {showMenu && <Menu />}
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <div className="header-container">
+        <div>
+          <img
+            className="header-logo"
+            src={logo}
+            alt="thumbnail of henna singh"
+          />
+          <p>Henna Singh</p>
+        </div>
+        <div>
+          {showMenu ? (
+            <CgClose
+              size={36}
+              color="#EA035E"
+              onClick={toggleMenu}
+              className="close-menu"
+            />
+          ) : (
+            <CgMenuRight
+              size={36}
+              color="#EA035E"
+              onClick={toggleMenu}
+              className="menu-right"
+            />
+          )}
+          <Menu isActive={showMenu} hideMenu={toggleMenu} />
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import aboutMe from '../assets/aboutMe.jpeg'
+import React from "react";
+import aboutMe from "../assets/aboutMe.jpeg";
 
 export default function Home() {
-
-    return (
-        <section className='home-hero'>
-            <div className='home-hero-content'>
-                <div>
-                    <h1 className='hero-heading'>Hey, Welcome to my Creative Nook!</h1>
-                    <p className='hero-text'>
-                        I am a passionate technical <span>community builder,</span>  educator, writer, 
-                        <span> speaker, developer</span> and a continuous learner based in Dublin, Ireland.
-                        I am a seasoned professional learning front-end development skills, <span>React JS</span> , HTML, CSS, and <span> Firebase.</span>
-                    </p>
-                    <a href='./#projects' className='projects-btn'>See my projects</a>  
-                </div>
-                <img src= {aboutMe} alt="henna standing at podium talking"/>
-            </div>   
-      
-        </section>
-    )
+  return (
+    <section className="home-hero" id="home">
+      <div className="home-hero-content">
+        <div>
+          <h1 className="hero-heading">Hey, Welcome to my Creative Nook!</h1>
+          <p className="hero-text">
+            I am a passionate technical <span>community builder,</span>{" "}
+            educator, writer,
+            <span> speaker, developer</span> and a continuous learner based in
+            Dublin, Ireland. I am a seasoned professional learning front-end
+            development skills, <span>React JS</span> , HTML, CSS, and{" "}
+            <span> Firebase.</span>
+          </p>
+          <a href="./#projects" className="projects-btn">
+            See my projects
+          </a>
+        </div>
+        <img src={aboutMe} alt="henna standing at podium talking" />
+      </div>
+    </section>
+  );
 }
