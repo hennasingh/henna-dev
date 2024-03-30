a67051e270cbfc859ede9fa255b1f9fe1f5d1dfc
Henna Singh <hennasingh04@gmail.com>
2024-03-30 14:41:13 +0530

    added overview and techstack in project card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,37 +1,42 @@
-import React from 'react'
-import { convertGsUrlToHttpUrl } from '../firebase'
+import React from "react";
+import { convertGsUrlToHttpUrl } from "../firebase";
 
-export default function ProjectCard({props}) {
-    const {image, title, description, projectUrl, githubUrl} = props
+export default function ProjectCard({ props }) {
+  const { image, title, overview, projectUrl, githubUrl, techStack } = props;
 
-    const [imageUrl, setImageUrl] = React.useState(null)
+  const [imageUrl, setImageUrl] = React.useState(null);
 
-    React.useEffect(() => {
-        async function fetchImage() {
-            try {
-                const httpUrl = await convertGsUrlToHttpUrl(image)
-                setImageUrl(httpUrl)
-            } catch(error) {
-                console.error("Error fetching image URL:", error)
-            }
-        }
-        fetchImage()
-    }, [image])
+  React.useEffect(() => {
+    async function fetchImage() {
+      try {
+        const httpUrl = await convertGsUrlToHttpUrl(image);
+        setImageUrl(httpUrl);
+      } catch (error) {
+        console.error("Error fetching image URL:", error);
+      }
+    }
+    fetchImage();
+  }, [image]);
 
-    
-    return (
-        <div className='project-container'>
-            <img src={imageUrl} alt={title} />
+  return (
+    <div className="project-container">
+      <img src={imageUrl} alt={title} />
 
-            <div className='project-body'>
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <div className='links'>
-                    <a href={githubUrl} target="_blank">Source Code</a>
-                    <a href={projectUrl} target="_blank">Live Demo</a>
-                </div>
-            </div>
-
+      <div className="project-body">
+        <h3>{title}</h3>
+        <p>{overview}</p>
+        <p>
+          <strong>Tech Stack:</strong> {techStack.join(", ")}
+        </p>
+        <div className="links">
+          <a href={githubUrl} target="_blank">
+            Source Code
+          </a>
+          <a href={projectUrl} target="_blank">
+            Live Demo
+          </a>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
